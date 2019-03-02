2790302502460b9dd4e78151879f05a1170d8178
nico.funke <nico.funke@stud.uni-goettingen.de>
2019-03-02 17:37:35 +0100

    New concert-detail design with details

--- src/app/components/concert-details/concert-details.component.ts
+++ src/app/components/concert-details/concert-details.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Concert } from "../../interfaces/concert.interface";
 import { ConcertService } from "../../services/concert.service";
-import { tap } from "rxjs/operators";
+import { tap, map, mergeMap, filter, distinctUntilChanged } from "rxjs/operators";
+import * as moment from "moment";
 
 @Component({
   selector: "glk-concert-details",
@@ -11,22 +12,45 @@ import { tap } from "rxjs/operators";
 })
 export class ConcertDetailsComponent implements OnInit {
 
-  public id: number;
+  /**
+   * Variables
+   */
   public concert: Concert;
+  public imageLink: string;
+  public formattedDate: string;
+
+  /**
+   * Observable for the id from route
+   */
+  concertIdFromRoute$ = this.route.params.pipe(
+    filter( params => !!params["id"]),
+    map( params => +params["id"]),
+    distinctUntilChanged()
+  );
+
+  /**
+   * Subscrition to the concertservice, that calls method to set components variables
+   */
+  concertSubscription = this.concertIdFromRoute$.pipe(
+    mergeMap( id => this.concertService.getConcertDetails( id ).pipe(
+      tap( concert =>  this.updateVariables( concert ) )
+    ) ),
+  ).subscribe();
 
   constructor( private route: ActivatedRoute,
               public concertService: ConcertService ) { }
 
-  /**
-   * Function that gets called on init and starts to subscribe the route params and fetch the right concert
-   */
   ngOnInit() {
-    this.route.params.subscribe(params => {
-      this.id = +params["id"];
-      this.concertService.getConcertDetails( this.id).pipe(
-        tap( concert => this.concert = concert),
-      ).subscribe();
-   });
+  }
+
+  /**
+   * Updates the components variables dependent on the given concert
+   * @param concert     concert to get the values from
+   */
+  updateVariables( concert: Concert): void {
+    this.concert = concert;
+    this.imageLink = this.concertService.URL_BACKEND + concert.picture;
+    this.formattedDate = moment( concert.date).format("DD.MM.YYYY");
   }
 
 }
