2b4a1b7374199b552148106cdab58e554c416da2
Nico Funke <funk-e@gross-schneen.de>
2019-03-03 18:49:33 +0100

    Added all icons to contact component

---
+++ src/app/interfaces/contact-links.interface.ts
@@ -0,0 +1,16 @@
+export interface ContactLinks {
+  readonly amazon?: string;
+  readonly android?: string;
+  readonly audio?: string;
+  readonly blog?: string;
+  readonly facebook?: string;
+  readonly instagram?: string;
+  readonly linkedin?: string;
+  readonly mail?: string;
+  readonly photo?: string;
+  readonly shopping?: string;
+  readonly twitter?: string;
+  readonly whatsapp?: string;
+  readonly website?: string;
+  readonly youtube?: string;
+}
