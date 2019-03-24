8d051a784efae1e5a6c8210240a10230df4ff474
Nico Funke <funk-e@gross-schneen.de>
2019-03-24 15:04:15 +0100

    Added image upload

--- src/app/interfaces/concert.interface.ts
+++ src/app/interfaces/concert.interface.ts
@@ -1,14 +1,12 @@
 export interface Concert {
-    readonly headline?: string;
     readonly bands: string;
     readonly date: string;
     readonly place: string;
     readonly doors: string;
     readonly begin: string;
-    readonly price?: string;
     readonly fblink: string;
     readonly buylink: string;
-    readonly picture: string;
+    picture: string;
     readonly description: string;
     readonly id?: string;
 }
--- src/app/views/concert-detail-view/concert-detail-view.component.ts
+++ src/app/views/concert-detail-view/concert-detail-view.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit, OnChanges, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, OnChanges } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Concert } from "../../interfaces/concert.interface";
 import { ConcertService } from "../../services/concert.service";
-import { tap, map, mergeMap, filter, distinctUntilChanged } from "rxjs/operators";
+import {
+  tap,
+  map,
+  mergeMap,
+  filter,
+  distinctUntilChanged
+} from "rxjs/operators";
 import * as moment from "moment";
 
 @Component({
@@ -10,41 +16,43 @@ import * as moment from "moment";
   templateUrl: "./concert-detail-view.component.html",
   styleUrls: ["./concert-detail-view.component.scss"]
 })
-export class ConcertDetailViewComponent implements OnInit {
-
-  /**
-   * Variables
-   */
+export class ConcertDetailViewComponent implements OnInit, OnChanges {
   public concert: Concert;
   public concertBackup: Concert;
-  public imageLink: string;
   public formattedDate: string;
-
-  /**
-   * ViewChilds to read elements content, that may been changed
-   */
-  @ViewChild("date") divDate: ElementRef;
-  @ViewChild("place") divPlace: ElementRef;
-  @ViewChild("doors") divDoors: ElementRef;
-  @ViewChild("begin") divBegin: ElementRef;
-  @ViewChild("bands") divBands: ElementRef;
-  @ViewChild("fbLink") divFbLink: ElementRef;
-  @ViewChild("buyLink") divBuyLink: ElementRef;
-  @ViewChild("description") divDescription: ElementRef;
-
-  // boolean, if the concert is currently editable
   public editMode = false;
 
-  test() {
-    // console.log(this.concert);
-    console.log(this.divBands.nativeElement.innerHTML);
-    console.log(this.divDate.nativeElement.innerHTML);
-    console.log(this.divPlace.nativeElement.innerHTML);
-    console.log(this.divDoors.nativeElement.innerHTML);
-    console.log(this.divBegin.nativeElement.innerHTML);
-    console.log(this.divDescription.nativeElement.innerHTML);
-    console.log(this.divFbLink.nativeElement.innerHTML);
-    console.log(this.divBuyLink.nativeElement.innerHTML);
+  /**
+   * Observable for the id from route
+   */
+  concertIdFromRoute$ = this.route.params.pipe(
+    filter(params => !!params["id"]),
+    map(params => +params["id"]),
+    distinctUntilChanged()
+  );
+
+  /**
+   * Subscrition to the concertservice, that calls method to set components variables
+   */
+  concertSubscription = this.concertIdFromRoute$
+    .pipe(
+      mergeMap(id =>
+        this.concertService
+          .getConcertDetails(id)
+          .pipe(tap(concert => this.updateVariables(concert)))
+      )
+    )
+    .subscribe();
+
+  constructor(
+    private route: ActivatedRoute,
+    public concertService: ConcertService
+  ) {}
+
+  ngOnInit() {}
+
+  ngOnChanges(): void {
+    console.log("changes");
   }
 
   /**
@@ -61,61 +69,64 @@ export class ConcertDetailViewComponent implements OnInit {
   cancelEdit(): void {
     this.editMode = false;
     this.updateVariables(this.concertBackup);
-
   }
 
   /**
    * Calls the service to delete the current concert
    */
   deleteConcert() {
-    this.concertService.deleteConcert( this.concert.id );
+    this.concertService.deleteConcert(this.concert.id);
   }
 
+  /**
+   * Sets the current concert to a new one and starts edit mode
+   */
   createNewConcert() {
     this.editMode = true;
     this.concert = this.concertService.getConcertTemplate();
   }
 
   /**
-   * Observable for the id from route
+   * Gets called when a new picture is chosen. Reads it as base64 to the current concert
+   * @param event     input even
    */
-  concertIdFromRoute$ = this.route.params.pipe(
-    filter( params => !!params["id"]),
-    map( params => +params["id"]),
-    distinctUntilChanged()
-  );
-
-  /**
-   * Subscrition to the concertservice, that calls method to set components variables
-   */
-  concertSubscription = this.concertIdFromRoute$.pipe(
-    mergeMap( id => this.concertService.getConcertDetails( id ).pipe(
-      tap( concert =>  this.updateVariables( concert ) )
-    ) ),
-  ).subscribe();
-
-  constructor( private route: ActivatedRoute,
-              public concertService: ConcertService ) { }
-
-  ngOnInit() {
+  changePicture(event): void {
+    const myReader: FileReader = new FileReader();
+    myReader.onloadend = () => {
+      this.concert.picture = myReader.result.toString();
+    };
+    myReader.readAsDataURL(event.target.files[0]);
   }
 
   /**
    * Updates the components variables dependent on the given concert
    * @param concert     concert to get the values from
    */
-  updateVariables( concert: Concert): void {
-    this.concert =  (JSON.parse(JSON.stringify(concert)));
-    this.concertBackup = (JSON.parse(JSON.stringify(concert)));
-    this.imageLink = this.concertService.URL_BACKEND + concert.picture;
-    this.formattedDate = moment( concert.date).format("DD.MM.YYYY");
+  updateVariables(concert: Concert): void {
+    this.concert = JSON.parse(JSON.stringify(concert));
+    this.concertBackup = JSON.parse(JSON.stringify(concert));
+    this.formattedDate = moment(concert.date).format("DD.MM.YYYY");
   }
 
   /**
    * Opens the given link in a new tab
    * @param link    link to open
    */
-  openLink( link: string) {
+  openLink(link: string) {
     window.open(link, "_blank");
+  }
+
+  /**
+   * Converts a picture string to a valid HTML src attribute
+   * @param picture   base64 image or backend link image
+   * @return src attribute to display in HTMl
+   */
+  pictureToSrcAttribute(picture: string): string {
+    if (!picture) {
+      return "";
+    } else if (picture.match(/data:image.*/gm)) {
+      return picture;
+    }
+    return this.concertService.URL_BACKEND + picture;
   }
 }
